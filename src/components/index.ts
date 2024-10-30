cb86c86ec38fa498d46db6a107047bad238f711e
chaotang <tc405259@alibaba-inc.com>
2024-10-30 19:21:35 +0800

    解决闪动问题

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 import Toc from './Toc';
 import VariableSizeList from './variable-size-list';
 import Up from './up';
+import CodeHighlight from './CodeHighlight';
 
 export {
   Toc,
   VariableSizeList,
+  CodeHighlight,
   Up,
 }
