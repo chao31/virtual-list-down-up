635908475a33d83f635cb7e87c13d0d9e70820c2
chao31 <cucfortc@163.com>
2024-10-30 22:06:29 +0800

    xxx

--- example/index.tsx
+++ example/index.tsx
@@ -1,9 +1,11 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import VariableSizeList, {
-  ExposeListData,
-} from '../src/components/VariableSizeList';
+// import VariableSizeList, {
+//   ExposeListData,
+// } from '../src/components/VariableSizeList';
+
+import { VariableSizeList } from 'virtual-list-down-up';
 import * as faker from 'faker';
 
 import './index.css';
