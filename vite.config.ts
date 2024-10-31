1ba231623658127174f37ffb6c97d3546a8efea2
chao31 <cucfortc@163.com>
2024-10-31 22:31:08 +0800

    配置

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,22 @@ export default defineConfig({
     },
   },
   plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'Index',
+      // the proper extensions will be added
+      fileName: 'index'
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['react'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          react: 'React'
+        }
+      }
+    }
+  }
 });
